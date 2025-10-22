7eb0d23ae923900f4704ad587d683a72b63f19a0
golsharifi <nima@orb.group>
2025-10-22 05:32:13 +0100

    feat: Update Android build configuration and dependencies for improved compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,27 +1,28 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.orbvpn.orbx"
-    compileSdk = 36  // Changed from 34 to 36
-    ndkVersion = "27.0.12077973"  // Changed from 25.1.8937393
+    compileSdk = 35  // Use 35 instead of 36
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17  // Changed from 1.8
+        targetCompatibility = JavaVersion.VERSION_17  // Changed from 1.8
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"  // Changed from 1.8
     }
 
     defaultConfig {
         applicationId = "com.orbvpn.orbx"
-        minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        minSdk = 24  // Set explicit minimum
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
     }
@@ -38,6 +39,5 @@ flutter {
 }
 
 dependencies {
-    // WireGuard for Android - KOTLIN SYNTAX
     implementation("com.wireguard.android:tunnel:1.0.20230427")
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.9.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -9,12 +21,13 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.layout.buildDirectory.set(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
